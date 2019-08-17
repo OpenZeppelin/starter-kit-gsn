2456cb1c11906a328846915b98645541e4bc32f3
Igor Yalovoy <igor@yalovoi.net>
2019-08-17 17:14:33 +0200

    Fix MNEMONIC name

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 const path = require("path");
-require('dotenv').config();
-const mnemonic = process.env.MNENOMIC;
+require("dotenv").config();
+const mnemonic = process.env.MNEMONIC;
 const infuraKey = process.env.INFURA_API_KEY;
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
@@ -11,36 +11,50 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*",
+      network_id: "*"
     },
     ropsten: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + infuraKey)
+        return new HDWalletProvider(
+          mnemonic,
+          "https://ropsten.infura.io/v3/" + infuraKey
+        );
       },
-      network_id: '3',
+      network_id: "3",
       gas: 4465030,
-      gasPrice: 10000000000,
+      gasPrice: 10000000000
     },
     kovan: {
       provider: function() {
-        return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + infuraKey)
+        return new HDWalletProvider(
+          mnemonic,
+          "https://kovan.infura.io/v3/" + infuraKey
+        );
       },
-      network_id: '42',
+      network_id: "42",
       gas: 4465030,
-      gasPrice: 10000000000,
+      gasPrice: 10000000000
     },
     rinkeby: {
-      provider: () => new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/" + infuraKey),
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://rinkeby.infura.io/v3/" + infuraKey
+        ),
       network_id: 4,
       gas: 3000000,
       gasPrice: 10000000000
     },
     // main ethereum network(mainnet)
     main: {
-      provider: () => new HDWalletProvider(mnemonic, "https://mainnet.infura.io/v3/" + infuraKey),
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          "https://mainnet.infura.io/v3/" + infuraKey
+        ),
       network_id: 1,
       gas: 3000000,
       gasPrice: 10000000000
     }
-  },
+  }
 };
