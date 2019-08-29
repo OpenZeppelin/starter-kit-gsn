8af00e45416244746f2a143b175b60dc1474b0e8
Igor Yalovoy <igor@yalovoi.net>
2019-08-29 23:51:37 +0300

    Update logo and email

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,21 +4,20 @@ import styles from './footer.module.scss';
 import mail from './mail.svg';
 import twitter from './twitter.svg';
 import github from './github.svg';
-import zeppelin from './zeppelin_logo.png';
+import zeppelin from './zeppelin_logo.svg';
 
 const Footer = () => (
   <footer className={styles.footer}>
     <div className={styles.brand}>
       <div className={styles.created}>
-        Created by
         <a href="https://openzeppelin.com/" rel="noopener noreferrer" target="_blank">
-          <img style={{ width: '80px', height: 'auto', marginLeft: '10px' }} src={zeppelin} alt="OpenZeppelin" />
+          <img style={{ width: '171px', height: 'auto', marginLeft: '10px' }} src={zeppelin} alt="OpenZeppelin" />
         </a>
       </div>
-      <div className={styles.copyright}>Copyright © 2019 zOS Global Limited</div>
+      <div className={styles.copyright}>Copyright © {new Date().getFullYear()} zOS Global Limited</div>
     </div>
     <div className={styles.links}>
-      <a href="mailto:ramon@zeppelin.solutions" target="_blank" rel="noopener noreferrer">
+      <a href="mailto:igor@openzeppelin.solutions" target="_blank" rel="noopener noreferrer">
         <img src={mail} alt="email" />
       </a>
       <a href="https://twitter.com/OpenZeppelin" rel="noopener noreferrer" target="_blank">
